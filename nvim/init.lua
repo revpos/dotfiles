d644f7c8b5304eede80ef55a052416409bb80721
Revanth Madupoju <revanthmadupoju@gmail.com>
2026-07-10 14:27:29 +0530

    cleanup ruff duplicate in LSPs and Mason list

--- nvim/init.lua
+++ nvim/init.lua
@@ -806,8 +806,6 @@ do
   local ensure_installed = vim.tbl_keys(servers or {})
   vim.list_extend(ensure_installed, {
     -- You can add other tools here that you want Mason to install
-    pyright,
-    ruff,
   })
 
   require('mason-tool-installer').setup { ensure_installed = ensure_installed }
